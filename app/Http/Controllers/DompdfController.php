c9e852774dd0da0fcb21ee488fe3c3b981cb3644
ashintai <156777132+ashintai@users.noreply.github.com>
2024-08-09 13:10:47 +0900

    PDF20

--- app/Http/Controllers/DompdfController.php
+++ app/Http/Controllers/DompdfController.php
@@ -22,57 +22,7 @@ class DompdfController extends Controller
 
        $pdf->setPaper('A4' , 'portrait');
         
-       // 必要最低限のフォントのみをPDFへ埋め込み設定
-    //    $pdf->setOptions([
-    //        'isFontSubsettingEnabled' => true, 
-    //        // 必要最低限のフォントのみを埋め込む
-    //        // 一時的にコメントアウト
-    //        // 'defaultFont' => 'ipag', // デフォルトフォントを設定
-    //    ]);
-
-
        return $pdf->stream('駐車券.pdf');
 
-       
-       
-        // $options = new Options();
-        // $options->set('isFontSubsettingEnabled' , true);
-        // $dompdf = new Dompdf($options);
-
-        // $html = view('dompdf.pdf')->render();
-
-        // $dompdf->loadHtml($html);
-        // $dompdf->setPaper('A4' , 'potrait');
-        // $dompdf->render();
-// AHO
-        // return $dompdf->stream('document.pdf');
-
-    // // return view( 'pdftest');
-    
-    //     // 画像データの準備
-    //     // $format = public_path('images/parkticket.jpg') ;
-    //     // $format_data = base64_encode(file_get_contents($format));
-    //     // その他の表示データ
-       
-       
-    //     $data = [
-    //         'name' => '山田太郎' , 
-    //         'zekken' => '123' , ];
-        
-    //     // Viewのdompdf/pdf.blade.php を使ってPDFを生成
-    //     $pdf = PDF::loadView('dompdf.pdf' , compact('data'));
-    //     // 用紙をA4タテに設定
-    //     $pdf->setPaper('A4' , 'portrait');
-        
-    //     // 必要最低限のフォントのみをPDFへ埋め込み設定
-    //     $pdf->setOptions([
-    //         'isFontSubsettingEnabled' => true, // 必要最低限のフォントのみを埋め込む
-    //         // 一時的にコメントアウト
-    //         // 'defaultFont' => 'ipag', // デフォルトフォントを設定
-    //     ]);
-
-    //     // PDFファイルを生成してWebへ表示
-    //     return $pdf->stream('駐車券.pdf');
-    // }
     }
 }
